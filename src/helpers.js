c28376b5fdae4d62d8c1e40bfdb71e17853184e2
Geoff Davis <geoffdavis92@gmail.com>
2017-10-16 00:09:13 -0500

    update stories

--- src/helpers.js
+++ src/helpers.js
@@ -6,4 +6,49 @@ const generateFilledArray = amount => {
 	return the_array;
 };
 
-export { generateFilledArray };
+const theOfficeData = [
+	{
+		name: "Jim Halpert",
+		username: "jhalpert",
+		email: "jim@athleap.com",
+		active: false
+	},
+	{
+		name: "Pam Halpert",
+		username: "phalpert",
+		email: "phalpert79@gmail.com",
+		active: false
+	},
+	{
+		name: "Ed Truck",
+		username: "edtruck",
+		email: "etruck@dundermifflin.com",
+		active: false
+	},
+	{
+		name: "Michael Scott",
+		username: "mscott",
+		email: "admin@michaelscottfoundation.org",
+		active: false
+	},
+	{
+		name: "Dwight Schrute",
+		username: "bearsbeats74",
+		email: "dschrute@dundermifflin.com",
+		active: true
+	},
+	{
+		name: "Phyllis Vance",
+		username: "pvance",
+		email: "pvance@dundermifflin.com",
+		active: true
+	},
+	{
+		name: "Angela Schrute",
+		username: "aschrute",
+		email: "aschrute@dundermifflin.com",
+		active: true
+	}
+];
+
+export { generateFilledArray, theOfficeData };
--- stories/index.js
+++ stories/index.js
@@ -1,37 +1,59 @@
-import React from 'react';
+import React from "react";
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { linkTo } from "@storybook/addon-links";
 
-import { Button, Welcome } from '@storybook/react/demo';
-import Floodgate from 'floodgate'
-import { ErrorMessage } from 'functions'
-import { generateFilledArray } from 'helpers'
+import { Button, Welcome } from "@storybook/react/demo";
+import Floodgate from "floodgate";
+import { ErrorMessage } from "functions";
+import { generateFilledArray, theOfficeData } from "helpers";
 
 const PreProps = {
 	style: {
-		backgroundColor: '#eee',
-		color: '#3a3a3a',
-		display: 'inline-block',
-		fontSize: '18px',
-		padding: '.66em'
+		backgroundColor: "#eee",
+		color: "#3a3a3a",
+		display: "inline-block",
+		fontSize: "18px",
+		padding: ".66em"
 	}
-}
+};
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf("Welcome", module).add("to Storybook", () => (
+	<Welcome showApp={linkTo("Button")} />
+));
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
-
-storiesOf('Floodgate', module)
-	.add('Numbers up to 100', () => {
-		const the_data = generateFilledArray(100)
-		return <Floodgate datasource={the_data}>{({ data }) => data.map(n => <p>{n}</p>)}</Floodgate>
-	})
-
-storiesOf('Utilities/functions/ErrorMessage', module)
-	.add('Generic message', () => <ErrorMessage text="Uncaught SyntaxError: {the error message}" {...PreProps} />)
-	.add('No `text` provided, with children', () => <ErrorMessage {...PreProps}>Uncaught SyntaxError: {'{the error message}'}</ErrorMessage>)
-	.add('No `text` provided, no children', () => <ErrorMessage />)
+storiesOf("Button", module)
+	.add("with text", () => (
+		<Button onClick={action("clicked")}>Hello Button</Button>
+	))
+	.add("with some emoji", () => (
+		<Button onClick={action("clicked")}>😀 😎 👍 💯</Button>
+	));
+storiesOf("Floodgate", module).add("Numbers up to 24", () => (
+	<Floodgate data={generateFilledArray(9)} stepCount={4}>
+		{({ data, loadNext, allLoaded }) => (
+			<article>
+				{data.map(n => <p key={n}>{n}</p>)}
+				{(!allLoaded && (
+					<p>
+						<button onClick={loadNext}>Load More</button>
+					</p>
+				)) || <p>All loaded.</p>}
+			</article>
+		)}
+	</Floodgate>
+));
+storiesOf("Utilities/functions/ErrorMessage", module)
+	.add("Generic message", () => (
+		<ErrorMessage
+			text="Uncaught SyntaxError: {the error message}"
+			{...PreProps}
+		/>
+	))
+	.add("No `text` provided, with children", () => (
+		<ErrorMessage {...PreProps}>
+			Uncaught SyntaxError: {"{the error message}"}
+		</ErrorMessage>
+	))
+	.add("No `text` provided, no children", () => <ErrorMessage />);
