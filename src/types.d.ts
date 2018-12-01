cde07d7d5b65fa88c40791868247e5ca6edde9c6
Geoff Davis <geoffdavis92@gmail.com>
2018-12-01 17:08:58 -0600

    add on* prop callbacks to type def file

--- src/types.d.ts
+++ src/types.d.ts
@@ -25,6 +25,9 @@ type FloodgateProps = {
   initial: number,
   saveStateOnUnmount?: boolean,
   exportState?: Function
+  onLoadNext?: Function,
+  onLoadComplete?: Function,
+  onReset?: Function
 };
 
 type FloodgateState = {
